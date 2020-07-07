dd37fcd597e938a8eb95c212f05df499c012ec8f
Michael Bragg <michael_bragg99@hotmail.com>
2020-07-06 18:13:12 -0600

    Initial commit for vendor-editor app.

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">Vendor Editor</div>
+    )
+  }
+}
+
+export default App;
