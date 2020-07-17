1a4f6eca3f891f56e9718efc62cc9637ef095aa9
Michael Bragg <michael_bragg99@hotmail.com>
2020-07-17 04:57:55 -0600

    Create expandable list component.

---
+++ src/Components/ExpandableList/ExpandableList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ExpandableList.css';
+
+class ExpandableList extends React.Component {
+  render() {
+    return (
+      <div className="ExpandableList">
+        EL
+      </div>
+    )
+  }
+}
+
+export default ExpandableList;
