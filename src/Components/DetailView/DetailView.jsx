7d963a3d50dd1ccf903880f051c1dc025ee2ae00
Michael Bragg <michael_bragg99@hotmail.com>
2020-07-17 05:14:46 -0600

    Add expanding and contracting functionality to expandableList component.

--- src/Components/DetailView/DetailView.jsx
+++ src/Components/DetailView/DetailView.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './DetailView.css';
 
+import ExpandableList from '../ExpandableList/ExpandableList';
+
 class DetailView extends React.Component {
   render() {
     return (
       <div className="DetailView">
         DetailView
+        <ExpandableList />
       </div>
     )
   }
--- src/Components/ExpandableList/ExpandableList.jsx
+++ src/Components/ExpandableList/ExpandableList.jsx
@@ -2,10 +2,38 @@ import React from 'react';
 import './ExpandableList.css';
 
 class ExpandableList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      expanded: false
+    }
+
+    this.setExpansion = this.setExpansion.bind(this);
+  }
+
+  setExpansion() {
+    const currentState = this.state.expanded;
+    this.setState({
+      expanded: !currentState
+    });
+  }
+
+  displayContents() {
+    return (
+      <div>
+        <p>contents</p>
+        <p>contents</p>
+        <p>contents</p>
+      </div>
+    )
+  }
+  
   render() {
     return (
       <div className="ExpandableList">
-        EL
+        {this.state.expanded ? this.displayContents : ''}
+        {this.state.expanded ? '-' : '+'}
       </div>
     )
   }
