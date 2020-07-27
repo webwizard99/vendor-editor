6090228b58c241e3ac6b94eac566484779e6b683
Michael Bragg <michael_bragg99@hotmail.com>
2020-07-27 16:08:56 -0600

    Edit editButton styles and add console log to detailReducer to trace program flow for editing existing item.

--- src/Components/EditButton/EditButton.jsx
+++ src/Components/EditButton/EditButton.jsx
@@ -5,7 +5,7 @@ class EditButton extends React.Component {
   render() {
     return (
       <div className="EditButton">
-        <div className="buttonIcon">edit</div>
+        <div className="editButtonIcon">edit</div>
       </div>
     )
   }
--- src/reducers/detailReducer.js
+++ src/reducers/detailReducer.js
@@ -12,6 +12,7 @@ export default function(state = initialState, action) {
       let newTargetId = action.payload.targetId;
       let newEdit = action.payload.edit;
       let newForm = action.payload.form;
+      console.log(newEdit);
       // if (!newTargetId && newTargetId !== 0) {
       //   newTargetId = state.targetId;
       // }
