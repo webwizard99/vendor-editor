e822ee50574c4bc71334f88e2c027addbba4d4e5
Michael Bragg <michael_bragg99@hotmail.com>
2020-07-27 10:47:17 -0600

    Add fields in potion display component.

--- src/Components/PotionDisplay/PotionDisplay.jsx
+++ src/Components/PotionDisplay/PotionDisplay.jsx
@@ -9,8 +9,39 @@ class PotionDisplay extends DisplayStatic {
   getDisplay() {
     const allPotions = this.props.potions;
     const thisPotion = allPotions.find(potion => potion.id === this.props.displayId);
-    console.log(thisPotion);
-    return thisPotion.item.name;
+    const name = thisPotion.item.name;
+    const value = thisPotion.item.value;
+    const details = thisPotion.item.details;
+    const potionType = thisPotion.type;
+    const level = thisPotion.level;
+    
+    return (
+      <div className="PotionDisplay">
+        <h2 className="display-heading">{name}</h2>
+        <div className="display-fields-area">
+          <div className="display-group">
+            <span className="display-label">Name</span>
+            <span className="display-text">{name}</span>
+          </div>
+          <div className="display-group">
+            <span className="display-label">Value</span>
+            <span className="display-number">{value}</span>
+          </div>
+          <div className="display-group">
+            <span className="display-label">Details</span>
+            <span className="display-text">{details}</span>
+          </div>
+          <div className="display-group">
+            <span className="display-label">Type</span>
+            <span className="display-text">{potionType}</span>
+          </div>
+          <div className="display-group">
+            <span className="display-label">Level</span>
+            <span className="display-number">{level}</span>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
