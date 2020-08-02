9e9da3cea5bbacf152667c7ad6e912fff3eecf8b
Michael Bragg <michael_bragg99@hotmail.com>
2020-08-02 11:06:53 -0600

    Add tracing in dialogReducer.

--- src/reducers/dialogReducer.js
+++ src/reducers/dialogReducer.js
@@ -18,6 +18,7 @@ const initialState = {
 export default function(state = initialState, action) {
   switch (action.type) {
     case SET_DIALOG:
+      console.log(action.payload);
       return {
         ...state,
         active: action.payload.active,
