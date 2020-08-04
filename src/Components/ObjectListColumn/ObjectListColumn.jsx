3875384fd24eaea6ab0f24372a06a4ef0f3d7881
Michael Bragg <michael_bragg99@hotmail.com>
2020-08-04 06:43:48 -0600

    Add check on object list column for completion of dialog box.

--- src/Components/ObjectListColumn/ObjectListColumn.jsx
+++ src/Components/ObjectListColumn/ObjectListColumn.jsx
@@ -8,12 +8,23 @@ import PotionsList from '../PotionsList/PotionsList';
 // redux imports
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
+import { SET_DETAIL_FORM } from '../../actions/types';
 
 class ObjectListColumn extends React.Component {
   constructor(props) {
     super(props);
 
     this.componentDidMount = this.componentDidMount.bind(this);
+  }
+
+  componentDidUpdate(nextProps) {
+    if (this.props.dialogActive && !this.nextProps.dialogActive) {
+      this.props.setDetailForm({
+        type: false,
+        targetId: null,
+        edit: false
+      })
+    }
   }
 
   componentDidMount() {
@@ -31,8 +42,20 @@ class ObjectListColumn extends React.Component {
 
 const mapStateToProps = state => {
   return {
-    potions: state.potions.potions
+    potions: state.potions.potions,
+    dialogActive: state.dialog.active
   }
 }
 
-export default connect(mapStateToProps, actions)(ObjectListColumn);
+const mapDispatchToProps = dispatch => {
+  return {
+    setDetailForm: (payload) => dispatch({ type: SET_DETAIL_FORM, payload: payload })
+  }
+}
+
+const allDispatches = {
+  ...mapDispatchToProps,
+  ...actions
+}
+
+export default connect(mapStateToProps, allDispatches)(ObjectListColumn);
