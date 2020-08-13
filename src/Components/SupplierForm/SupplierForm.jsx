4a331f32541a3aaf155402c310e0b5b22f973360
Michael Bragg <michael_bragg99@hotmail.com>
2020-08-13 15:41:27 -0600

    Fix issue with trying to map itemTypes object in SupplierForm component.

--- src/Components/SupplierForm/SupplierForm.jsx
+++ src/Components/SupplierForm/SupplierForm.jsx
@@ -35,7 +35,8 @@ class SupplierForm extends DisplayForm {
   }
 
   getOfferingOptions() {
-    return itemTypes.map(itemType => {
+    const itemArr = Object.values(itemTypes);
+    return itemArr.map(itemType => {
       return <option value={itemType}>{itemType}</option>
     })
   }
@@ -68,11 +69,33 @@ class SupplierForm extends DisplayForm {
               <input type="text" name="name" id="name" className="input-text" placeholder="weapon name"
                 maxLength="26" defaultValue={newName}></input>
             </div>
+            <div className="input-group-blank">
+            </div>
             <div className="offerings-form-area">
               <span className="item-label full-span">Offerings</span>
               <span className="item-label half-span">Type</span>
               <span className="item-label half-span">Markup</span>
-              
+              {offerings.map(offering => {
+                return (
+                  <div className="inner-span">
+                    <div className="half-span left-half">
+                      {/* <label className="item-label" htmlFor={`offering-${offering.id}-type`}>
+                        Type
+                      </label> */}
+                      <select className="offering-select" name={`offering-${offering.id}-type`} id={`offering-${offering.id}-type`} defaultValue={offering.type}>
+                        {this.getOfferingOptions()}
+                      </select>
+                    </div>
+                    <div className="half-span right-half">
+                      {/* <label className="item-label" htmlFor={`markup-${offering.id}-type`}>
+                        Markup
+                      </label> */}
+                      <input className="input-number" type="number" name={`markup-${offering.id}-type`} id={`markup-${offering.id}-type`} defaultValue={offering.markup}>
+                      </input>
+                    </div>
+                  </div>
+                )
+              })}
             </div>
         </form>
       </div>
