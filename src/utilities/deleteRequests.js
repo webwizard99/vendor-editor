704434d137aecd2b1fed8f4cff1104bfbf6cf339
Michael Bragg <michael_bragg99@hotmail.com>
2020-08-14 13:29:31 -0600

    Update signature of deleteRequests to attempt to forward a response from module.

--- src/utilities/deleteRequests.js
+++ src/utilities/deleteRequests.js
@@ -3,7 +3,13 @@ const deleteRequests = (function(){
     makeRequest: async function(deleteType, deleteId) {
       const deleteRes = await fetch(`/${deleteType}/${deleteId}`, {
         method: 'DELETE'
-      });
+      })
+        .then(response => response.json())
+        .then(data => {
+          return data
+        });
+
+      console.log(deleteRes);
       return deleteRes;
     }
   }
