e75f93f24d896c44521af40ca710b4226a0042f8
Michael Bragg <michael_bragg99@hotmail.com>
2020-08-15 07:05:36 -0600

    Remove count state from potionsReducer.

--- src/reducers/potionsReducer.js
+++ src/reducers/potionsReducer.js
@@ -1,22 +1,15 @@
 import { SET_POTIONS } from '../actions/types';
 
 const initialState = {
-  potions: null,
-  count: 0
+  potions: null
 }
 
 export default function(state = initialState, action) {
   switch (action.type) {
     case SET_POTIONS:
-      let arrlen = 0;
-      if (Array.isArray(action.payload)) {
-        arrlen = action.payload.length;
-      }
-      console.log(arrlen);
       return {
         ...state,
-        potions: action.payload,
-        count: arrlen
+        potions: action.payload
       }
     default:
       return state;
