2352da0f834137252aa8d2866910aaf8db2bfaba
Michael Bragg <michael_bragg99@hotmail.com>
2020-08-18 13:25:21 -0600

    Bind handleNew in WeaponsList component.

--- src/Components/WeaponsList/WeaponsList.jsx
+++ src/Components/WeaponsList/WeaponsList.jsx
@@ -16,6 +16,7 @@ class WeaponsList extends ExpandableList {
     this.displayContents = this.displayContents.bind(this);
     this.getNewButton = this.getNewButton.bind(this);
     this.componentDidMount = this.componentDidMount.bind(this);
+    this.handleNew = this.handleNew.bind(this);
   }
 
   componentDidMount() {
